dde2139c826269c9077fc796407cdd5f95944451
HarryW2125 <hjw235@exeter.ac.uk>
2026-02-14 11:16:27 +0000

    Finished task 3

--- StringFormatApp.java
+++ StringFormatApp.java
@@ -3,7 +3,17 @@ public class StringFormatApp{
         String name =new String(args[0]);
         int age = Integer.parseInt(args[1]);
         float height = Float.parseFloat(args[2]);
-        System.out.println("name: "+name+" Age: "+age+" Height: "+height +"m");
+        double spaceHeight = height * 100 *1.023;
+        long marsAge = Math.round(age/1.88);
+
+        String nameOutput = String.format("%-20s",name);
+        String ageOutput = String.format("%-3d",age);
+        String heightOutput = String.format("%-3.2f",height);
+        String spaceOutput = String.format("%.1f",spaceHeight);
+
+        System.out.println("name: "+nameOutput + " Age: "+ageOutput + " Height: "+heightOutput+"m");
+        System.out.println("My space height would be " + spaceOutput +"cm");
+        System.out.println("On Mars I would be approximately " + marsAge + " years old.");
 
     }
 }
